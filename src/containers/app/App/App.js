8d432e6a39a49a4fd744ae3bc0ced96053d01373
Jhovahn Gibbs <nhavohj@gmail.com>
2018-10-10 17:33:02 -0700

    commit add user input box

---
+++ src/containers/app/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        User Input: <span />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default App;
