51982f0f9b8f7dbe27ba802aa82c293c255ef4ad
Jhovahn Gibbs <nhavohj@gmail.com>
2018-10-18 12:59:42 -0700

    add sentiment rating and bootstrap

--- src/containers/app/App/App.js
+++ src/containers/app/App/App.js
@@ -14,6 +14,15 @@ const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
 
 class App extends Component {
+  wordScore(score) {
+    if (score === 0) return 'NEUTRAL';
+    if (score > 0 && score < 1) return 'SLIGHTLY POSITIVE';
+    if (score >= 1 && score < 3) return 'POSITIVE';
+    if (score >= 3) return 'EXTREMELY POSITIVE';
+    if (score < 0 && score > -1) return 'SLIGHTLY NEGATIVE';
+    if (score < -1 && score > -3) return 'NEGATIVE';
+    if (score <= -3) return 'EXTREMELY NEGATIVE';
+  }
   render() {
     const {
       inputSuccess,
@@ -38,14 +47,23 @@ class App extends Component {
         ? sent.map(el => el.analysis.score).reduce((a, b) => a + b) /
           sent.length
         : false;
+    const word = this.wordScore(score);
 
     return (
-      <div>
-        <h2>Twitter Handle + ENTER for Sentiment Analysis</h2>
+      <div className="text-center">
+        <h1 />
+        <h2>Sentiment Analysis of latest Tweets by Twitter Handle</h2>
         <form onSubmit={onUserSubmit}>
           <input type="text" value={input} onChange={onUserInput} />
           <h2>@{input}</h2>
-          {score ? <h2>Sentiment Score: {score}</h2> : ''}
+          {score ? (
+            <div>
+              <h3>Average Sentiment Score: {score}</h3>
+              <h3>Rating: {word}</h3>
+            </div>
+          ) : (
+            ''
+          )}
         </form>
         {inputSuccess.length ? <List list={inputSuccess} /> : ''}
       </div>
